1791f40f3da51a4ed66b659947661bacdadf23ad
apon995 <shofikulsamz995@gmail.com>
2023-11-08 19:11:35 +0600

    create details paage and desin

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Navbar from './Components/Navbar.jsx';
 import Footer from './Components/Footer.jsx';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import './Main.css';
 import Banner from './Components/Banner.jsx';
 
 function App() {
-  const location = useLocation();
 
 
 
-  useEffect(() => {
-    
-  }, [location.pathname])
+
+
 
   return (
     <>
       <header>
-        <div id='banner'>
-          <Banner />
-        </div>
+
+        <Banner />
         <Navbar />
       </header>
       <main className='min-h-screen'>
--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,31 +1,165 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 function Banner() {
     const location = useLocation();
 
+    useEffect(() => {
+        if (location.pathname == '/') {
+
+            document.getElementById('banner').classList.remove('room')
+            document.getElementById('banner').classList.remove('superior')
+            document.getElementById('banner').classList.remove('signature')
+            document.getElementById('banner').classList.remove('luxary')
+            document.getElementById('banner').classList.remove('deluxe')
+            document.getElementById('banner').classList.remove('mybooking')
+            document.getElementById('banner').classList.remove('about')
+            document.getElementById('banner').classList.remove('contact')
+
+            document.getElementById('banner').classList.add('home')
+
+        }
+        else if (location.pathname == '/Room') {
+            document.getElementById('banner').classList.remove('superior')
+            document.getElementById('banner').classList.remove('signature')
+            document.getElementById('banner').classList.remove('luxary')
+            document.getElementById('banner').classList.remove('deluxe')
+            document.getElementById('banner').classList.remove('room')
+            document.getElementById('banner').classList.remove('mybooking')
+            document.getElementById('banner').classList.remove('about')
+            document.getElementById('banner').classList.remove('home')
+            document.getElementById('banner').classList.add('room')
+
+        }
+        else if (location.pathname == '/Mybooking') {
+            document.getElementById('banner').classList.remove('superior')
+            document.getElementById('banner').classList.remove('signature')
+            document.getElementById('banner').classList.remove('luxary')
+            document.getElementById('banner').classList.remove('deluxe')
+            document.getElementById('banner').classList.remove('home')
+            document.getElementById('banner').classList.remove('room')
+            document.getElementById('banner').classList.remove('mybooking')
+            document.getElementById('banner').classList.remove('about')
+            document.getElementById('banner').classList.add('mybooking')
+
+        }
+        else if (location.pathname == '/About') {
+            document.getElementById('banner').classList.remove('superior')
+            document.getElementById('banner').classList.remove('signature')
+            document.getElementById('banner').classList.remove('luxary')
+            document.getElementById('banner').classList.remove('deluxe')
+            document.getElementById('banner').classList.remove('home')
+            document.getElementById('banner').classList.remove('room')
+            document.getElementById('banner').classList.remove('mybooking')
+            document.getElementById('banner').classList.remove('contact')
+            document.getElementById('banner').classList.add('about')
+
+        }
+        else if (location.pathname == '/Contact') {
+            document.getElementById('banner').classList.remove('superior')
+            document.getElementById('banner').classList.remove('signature')
+            document.getElementById('banner').classList.remove('luxary')
+            document.getElementById('banner').classList.remove('deluxe')
+            document.getElementById('banner').classList.remove('home')
+            document.getElementById('banner').classList.remove('room')
+            document.getElementById('banner').classList.remove('mybooking')
+            document.getElementById('banner').classList.remove('about')
+            document.getElementById('banner').classList.add('contact')
+
+        }
+        else if (location.pathname == '/Details/Superior%20Room') {
+
+            document.getElementById('banner').classList.remove('home')
+            document.getElementById('banner').classList.remove('room')
+            document.getElementById('banner').classList.remove('mybooking')
+            document.getElementById('banner').classList.remove('about')
+            document.getElementById('banner').classList.remove('contact')
+            document.getElementById('banner').classList.add('superior')
+
+
+        }
+        else if (location.pathname == '/Details/Deluxe%20Room') {
+            document.getElementById('banner').classList.remove('home')
+            document.getElementById('banner').classList.remove('room')
+            document.getElementById('banner').classList.remove('mybooking')
+            document.getElementById('banner').classList.remove('about')
+            document.getElementById('banner').classList.remove('contact')
+            document.getElementById('banner').classList.remove('superior')
+            document.getElementById('banner').classList.add('deluxe')
+
+        }
+        else if (location.pathname == '/Details/Signature%20Room') {
+            document.getElementById('banner').classList.remove('home')
+            document.getElementById('banner').classList.remove('room')
+            document.getElementById('banner').classList.remove('mybooking')
+            document.getElementById('banner').classList.remove('about')
+            document.getElementById('banner').classList.remove('contact')
+            document.getElementById('banner').classList.remove('deluxe')
+            document.getElementById('banner').classList.remove('superior')
+            document.getElementById('banner').classList.add('signature')
+
+        }
+
+        else if (location.pathname == '/Details/Luxury%20Suites%20Room') {
+            document.getElementById('banner').classList.remove('home')
+            document.getElementById('banner').classList.remove('room')
+            document.getElementById('banner').classList.remove('mybooking')
+            document.getElementById('banner').classList.remove('about')
+            document.getElementById('banner').classList.remove('contact')
+            document.getElementById('banner').classList.remove('deluxe')
+            document.getElementById('banner').classList.remove('superior')
+            document.getElementById('banner').classList.remove('signature')
+            document.getElementById('banner').classList.add('luxary')
+
+
+
+        }
+
+
+
+
+
+
+
+    }, [location.pathname])
+
     return (
         <>
-            <div className={`flex text-start items-center flex-col justify-center w-fit h-full text-[#FFFF] px-[5%]`}>
-                <span className={`${location.pathname == '/' ? 'block' : 'hidden'} text-start w-full text-base font-normal uppercase`}>Live The exprience</span>
+            <div id='banner' className='BannerBackground'>
+                <div className={`flex text-start items-center flex-col justify-center w-fit h-full text-[#FFFF] px-[5%]`}>
+                    <span className={`${location.pathname == '/' ? 'block' : 'hidden'} text-start w-full text-base font-normal uppercase`}>Live The exprience</span>
 
-                <h1 className={`${location.pathname == '/' ? 'block' : 'hidden'} text-8xl leading-tight uppercase font-bold`}>Make <br />
-                    yourself <br />
-                    At the Home</h1>
+                    <h1 className={`${location.pathname == '/' ? 'block' : 'hidden'} text-8xl leading-tight uppercase font-bold`}>Make <br />
+                        yourself <br />
+                        At the Home</h1>
 
-                <h1 className={`${location.pathname == '/About' ? 'block' : 'hidden'} text-8xl font-bold text-white`}>
-                    About <br /> our self
-                </h1>
+                    <h1 className={`${location.pathname == '/About' ? 'block' : 'hidden'} text-8xl font-bold text-white`}>
+                        About <br /> our self
+                    </h1>
 
-                <h1 className={`${location.pathname == '/Contact' ? 'block' : 'hidden'} text-8xl font-bold text-white`}>
-                    Contact <br /> With us
-                </h1>
+                    <h1 className={`${location.pathname == '/Contact' ? 'block' : 'hidden'} text-8xl font-bold text-white`}>
+                        Contact <br /> With us
+                    </h1>
 
-                <div className={`${location.pathname == '/' ? 'block' : 'hidden'} w-full text-start`}>
-                    <button className='border-2 py-4 px-6 text-base active:bg-[#ff0000] active:text-[#FFFF] hover:bg-white hover:text-black transition-all font-medium'>View our rooms</button>
+                    <h1 className={`${location.pathname == '/Details/Superior%20Room' ? 'block' : 'hidden'}  uppercase text-8xl font-bold text-white`}>
+                        Superior <br /> Room
+                    </h1>
+                    <h1 className={`${location.pathname == '/Details/Deluxe%20Room' ? 'block' : 'hidden'}  uppercase text-8xl font-bold text-white`}>
+                        Deluxe <br /> Room
+                    </h1>
+                    <h1 className={`${location.pathname == '/Details/Signature%20Room' ? 'block' : 'hidden'}  uppercase text-8xl font-bold text-white`}>
+                        signature <br /> Room
+                    </h1>
+                    <h1 className={`${location.pathname == '/Details/Luxury%20Suites%20Room' ? 'block' : 'hidden'}  uppercase text-8xl font-bold text-white`}>
+                        Luxury <br /> Room
+                    </h1>
+
+                    <div className={`${location.pathname == '/' ? 'block' : 'hidden'} w-full text-start`}>
+                        <button className='border-2 py-4 px-6 text-base active:bg-[#ff0000] active:text-[#FFFF] hover:bg-white hover:text-black transition-all font-medium'>View our rooms</button>
+                    </div>
                 </div>
-            </div>
 
+            </div>
 
 
 
